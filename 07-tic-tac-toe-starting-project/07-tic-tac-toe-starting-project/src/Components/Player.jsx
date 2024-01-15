14bacd398f0dd4c51745318d5c42b628994fb9fc
pranitrathod <pranitrathodsocial@gmail.com>
2024-01-15 20:58:32 +0530

    Created a button with Save and edit alternatives changes!

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/Components/Player.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/Components/Player.jsx
@@ -2,33 +2,33 @@ import { useState } from "react";
 
 export default function Player({ name = "Computer", symbol }) {
     const [isEditing,setEdit]=useState(false);
+    const [Edit,save]=useState("Edit");
     function edit()
     {console.log("inside!");
         if(!isEditing){
             setEdit(true);
+            save("Save");
         }else{
             setEdit(false);
+            save("Edit");
         }
     }
-    const [name,changedName]=useState({name});
-    
-    function change()
-    {
-        if(setEdit){
-            changedName()
-        }
-    }
-
-
+    // const [name,changedName]=useState({name});
+    // function change()
+    // {
+    //     if(setEdit){
+    //         changedName()
+    //     }
+    // }
   return (
     <li>
       <span className="player">
-      {isEditing?<input type="text" onChange={change}/>:<span className="player-name">
+      {isEditing?<input type="text"/>:<span className="player-name">
           {name}
-        </span>}
-        
+        </span>
+      }
         <span className="player-symbol">{symbol}</span>
-        <button onClick={edit}>Edit</button>
+        <button onClick={edit}>{Edit}</button>
       </span>
     </li>
   );
