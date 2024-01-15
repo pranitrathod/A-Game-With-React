d50269d88f607a32ad6eb6fd67cb21f704593f2e
pranitrathod <pranitrathodsocial@gmail.com>
2024-01-15 21:46:51 +0530

    Two-ways bind!

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/Components/Player.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/Components/Player.jsx
@@ -2,33 +2,35 @@ import { useState } from "react";
 
 export default function Player({ name = "Computer", symbol }) {
     const [isEditing,setEdit]=useState(false);
-    const [Edit,save]=useState("Edit");
+    const [Edit,save]=useState(false);
+    const [playerName,setPlayerName]=useState(name);
     function edit()
-    {console.log("inside!");
+    {   console.log("inside!");
         if(!isEditing){
-            setEdit(true);
-            save("Save");
+            setEdit(true); 
         }else{
             setEdit(false);
-            save("Edit");
         }
+        save((editIt)=>!editIt);
+        //here dont get confused why the variable name is different,
+        //whenever anything changes it will change to Edit!    
+    } 
+    function handleChange(eventKey)
+    {
+    if(Edit)
+    {
+      setPlayerName(eventKey.target.value);
+    } 
     }
-    // const [name,changedName]=useState({name});
-    // function change()
-    // {
-    //     if(setEdit){
-    //         changedName()
-    //     }
-    // }
   return (
     <li>
       <span className="player">
-      {isEditing?<input type="text"/>:<span className="player-name">
-          {name}
-        </span>
+      {isEditing?<input type="text" required defaultValue={playerName} onChange={handleChange}/>:<span className="player-name">
+          {playerName}
+        </span>//Above change is also called as two-way binding when onchange triggers useState changes 
       }
-        <span className="player-symbol">{symbol}</span>
-        <button onClick={edit}>{Edit}</button>
+      <span className="player-symbol">{symbol}</span>
+      <button onClick={edit}>{Edit?"Save":"Edit"}</button>
       </span>
     </li>
   );
