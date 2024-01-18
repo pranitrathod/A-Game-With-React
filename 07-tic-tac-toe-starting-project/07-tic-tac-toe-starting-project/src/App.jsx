6c03946072b723388088fb4ff9888c6b2a65dc06
pranitrathod <pranitrathodsocial@gmail.com>
2024-01-18 19:56:57 +0530

    Revert "Learned lifting up states!"
    This reverts commit a87a8c2728233d67a872c36224b01de238171d79.

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,21 +1,13 @@
 import Player from "./Components/Player";
-import GameBoard from "./Components/GameBoard";
-import { useState } from "react";
 
 function App() {
-  const [active,setActive]=useState("X");
-  
-  function putYourSymbol()
-  {
-    setActive((player)=>player==="X" ? "O" :"X");
-  }
   return (
     <main id="game-container">
-      <ol className="highlight-player" id="players">
-      <Player  isActive={active==="X"} name="Player 1" symbol="X"/>
-      <Player  isActive={active==="O"} name="Player 2" symbol="O"/>
+      <ol id="players">
+      <Player name="Player 1" symbol="X"/>
+      <Player name="Player 2" symbol="O"/>
       </ol>
-      <GameBoard onSelectSquare={putYourSymbol} activePlayer={active}/>
+     
     </main>
   );
 }
