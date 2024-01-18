0d08cf68bd88ebc9341becc38cd263f343cc6119
pranitrathod <pranitrathodsocial@gmail.com>
2024-01-18 17:49:42 +0530

    Revert "Learned lifting up states!"
    This reverts commit cfac315ffbc8c80fe8a789e132f79bccde01fc3b.

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <Player name="Player 1" symbol="X"/>
       <Player name="Player 2" symbol="O"/>
       </ol>
-      <GameBoard onSelectSquare={whoIsActive} activePlayer={active}/>
+     
     </main>
   );
 }
